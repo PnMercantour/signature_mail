7a5e90a85f42ea3bc1c9de1ec3125c4a639e717c
QuentinVintras <quentin.vintras@ensta-bretagne.org>
2025-10-21 17:13:53 +0200

    new signature v_final

---
+++ config.js
@@ -0,0 +1,16 @@
+var CONFIG = {
+    logo: 'https://media.mercantour.eu/sign_img/logo_signature_reduit.png',
+    lien_parc: 'https://mercantour-parcnational.fr',
+    points: 'http://pnx.espaces-naturels.fr/signature/pointilles_pnm.png',
+    parc: 'Parc national du Mercantour',
+    adresse: '23 rue d\'Italie, CS 51316, 06006 Nice Cedex 1',
+
+    tel: 'Tel : +33 (0)4 93 16 78 XX - Mobile : +33 (0)6 34 47 68 XX',
+    facebook: '<a href="https://www.facebook.com/ParcnationalduMercantour"><img src="https://media.mercantour.eu/sign_img/signature_facebook.png" alt="" id="Facebook" style="BORDER:0px"/></a>',
+    youtube: '<A href="https://www.youtube.com/user/MercantourTV"><IMG src="https://media.mercantour.eu/sign_img/signature_youtube.png" alt="" id="YouTube" style="BORDER:0px" /></A>',
+    instagram: '<A href="https://www.instagram.com/pnmercantour/"><IMG src="https://media.mercantour.eu/sign_img/signature_instagram.png" alt="" id="Instagram" style="BORDER:0px"></A>',
+    linkedin: '<A href="https://fr.linkedin.com/company/parc-national-du-mercantour"><IMG src="https://media.mercantour.eu/sign_img/signature_linkedin.png" alt="" id="LinkedIn" style="BORDER:0px"></A>',
+    twitter: '<A href="https://twitter.com/mercantour"><IMG src="https://media.mercantour.eu/sign_img/signature_twitter.png" alt="" id="Twitter" style="BORDER:0px" /></A>',
+    templates: ['classique']
+    //pinterest: '<A href="https://www.pinterest.com/XXXXXXXXXX"><IMG src="http://pnx.espaces-naturels.fr/signature/signature_pinterest.png" alt="" id="Pinterest" style="BORDER:0px"></A>',
+}
